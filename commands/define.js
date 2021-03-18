246a6cd5443787c8da7c9403126eb5b65c6cbcd9
Levi <lhalvorson34@gmail.com>
2021-03-18 11:25:55 -0600

    changed define to show numbered list

--- commands/define.js
+++ commands/define.js
@@ -15,17 +15,46 @@ module.exports = function (msg, args) {
   axios
     .request(options)
     .then(function (response) {
+      console.log(response.data.meaning);
       if (response.data.meaning.noun) {
-        msg.channel.send(response.data.meaning.noun);
+        let newNoun = response.data.meaning.noun.split(/[()]+/);
+        let count = 1;
+        newNoun.forEach((element) => {
+          if (element !== "nou" && element !== "") {
+            msg.channel.send(`Noun ${count}.) ` + element);
+            count++;
+          }
+        });
       }
       if (response.data.meaning.verb) {
-        msg.channel.send(response.data.meaning.verb);
+        let newVerb = response.data.meaning.verb.split(/[()]+/);
+        let count = 1;
+        newVerb.forEach((element) => {
+          if (element !== "vrb" && element !== "") {
+            msg.channel.send(`Verb ${count}.) ` + element);
+            count++;
+          }
+        });
       }
       if (response.data.meaning.adverb) {
-        msg.channel.send(response.data.meaning.adverb);
+        let newAdverb = response.data.meaning.adverb.split(/[()]+/);
+        let count = 1;
+        newAdverb.forEach((element) => {
+          if (element !== "adv" && element !== "") {
+            msg.channel.send(`Adverb ${count}.) ` + element);
+            count++;
+          }
+        });
       }
       if (response.data.meaning.adjective) {
-        msg.channel.send(response.data.meaning.adjective);
+        let newAdjective = response.data.meaning.adjective.split(/[()]+/);
+        let count = 1;
+        newAdjective.forEach((element) => {
+          if (element !== "adj" && element !== "") {
+            msg.channel.send(`Adjective ${count}.) ` + element);
+            count++;
+          }
+        });
       }
     })
     .catch(function (error) {
